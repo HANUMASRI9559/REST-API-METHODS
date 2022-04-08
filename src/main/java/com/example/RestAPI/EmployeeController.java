2c9db059ca04fd4ef7457492d9418baef86ad752
Hanumasri Naramamidi <inhnara@KMC.Corp>
2022-04-08 10:28:30 +0530

    firstcommit

--- src/main/java/com/example/RestAPI/EmployeeController.java
+++ src/main/java/com/example/RestAPI/EmployeeController.java
@@ -1,10 +1,9 @@
 package com.example.RestAPI;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +18,24 @@ public class EmployeeController {
 
 	private static final String name = null;
 
-	// http://localhost:9099/name=max
+	// http://localhost:9099/Get
 	@RequestMapping(value = "/Get", method = RequestMethod.GET)
 	public String Employeedetails(@RequestParam(value = "name", defaultValue = "KMC") String name) {
 		return "Employee" + name;
 	}
 
-	@RequestMapping(value = "/Create", method = RequestMethod.POST)
-	public String Employeedetails1(@RequestBody String name , String id) {
+	@PostMapping(value = "/Create")
+	public String Employeedetails1(@RequestBody String name) {
 		return "Employee" + name;
 	}
 
 	@PutMapping(value = "/Update")
-	public String Employeedetails11(@RequestBody String name , String id) {
+	public String Employeedetails11(@RequestBody String name) {
 		return "Employee" + name;
 	}
 
 	@DeleteMapping(value = "/Delete")
-	public String Employeedetails111(@RequestBody String name, String id) {
+	public String Employeedetails111(@RequestBody String name) {
 		return "MESSAGE DELETED....";
 	}
 
